3ca9203def96393cef0bd51e98e1da9efaed526f
Yego <eddieyego273@gmail.com>
2025-05-31 22:01:02 +0300

    added an execution script for list_reviews_by_books

--- app/cli.py
+++ app/cli.py
@@ -1,3 +1,27 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from models import Review, Book, User, Genre
+
+DATABASE_URL = "sqlite:///page_1.db"
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+
+def list_reviews_by_books(book_id):
+    session = Session()
+    reviews = session.query(Review).filter_by(book_id=book_id).all()
+
+    if reviews:
+        book =session.query(Book).filter_by(id=book_id).first()
+        print(f"\nReview for Book: {book.title}\n{'-'*40}")
+        for r in reviews:
+            user = session.query(User).filter_by(id=r.user_id).first()
+            print(f" - {user.name} rated it {r.rating}/5: {r.review}")
+        avg_rating = sum(r.rating for r in reviews) / len(reviews)
+        print(f"\nAverage Rating: {avg_rating:.1f}/5\n{'-'*40}")
+    else:
+        print("No reviews found or invalid book ID.")
+    
+    session.close()
+
+def list_reviews_by_users(user_id):
+    
